9af46eb395b29dd0e4e7e556ddee8c71660544ff
Udazi Ephraim Oseiwe <udazi.osebest@gmail.com>
2026-03-26 13:46:12 +0100

    feat: setup resources, fonts and files for the project

---
+++ app/home/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-medium underline">Hello world!</h1>
+      <div>
+        <p className="text-lg text-gray-500">
+          This is a sample Next.js app with Tailwind CSS.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
